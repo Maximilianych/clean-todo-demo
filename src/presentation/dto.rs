20fd877a0b96da906a12aa47674bc75ea8d1a145
Maximilianych <maxgorochov27@gmail.com>
2025-07-14 01:01:43 +0300

    Отправка сообщений об ошибке в виде json

--- src/presentation/dto.rs
+++ src/presentation/dto.rs
@@ -1,4 +1,6 @@
-use crate::domain::entities::Task;
+use actix_web::HttpResponse;
+
+use crate::{application::services::TaskServiceError, domain::entities::Task};
 
 #[derive(serde::Deserialize)]
 pub struct CreateTaskRequest {
@@ -21,6 +23,53 @@ impl From<Task> for TaskResponse {
             title: task.title,
             description: task.description,
             status: task.status,
+        }
+    }
+}
+
+#[derive(serde::Serialize)]
+pub struct ApiErrorResponse {
+    pub code: String,
+    pub message: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub details: Option<serde_json::Value>,
+}
+
+impl From<TaskServiceError> for HttpResponse {
+    fn from(e: TaskServiceError) -> Self {
+        let response = match e {
+            TaskServiceError::MissingTitle => ApiErrorResponse {
+                code: "validation_failed".to_string(),
+                message: "Title is required".to_string(),
+                details: None,
+            },
+            TaskServiceError::MissingDescription => ApiErrorResponse {
+                code: "validation_failed".to_string(),
+                message: "Description is required".to_string(),
+                details: None,
+            },
+            TaskServiceError::TaskNotFound => ApiErrorResponse {
+                code: "not_found".to_string(),
+                message: "Task not found".to_string(),
+                details: None,
+            },
+            TaskServiceError::TaskAlreadyExists => ApiErrorResponse {
+                code: "conflict".to_string(),
+                message: "Task already exists".to_string(),
+                details: None,
+            }
+        };
+
+        match e {
+            TaskServiceError::MissingTitle | TaskServiceError::MissingDescription => {
+                HttpResponse::BadRequest().json(response)
+            },
+            TaskServiceError::TaskNotFound => {
+                HttpResponse::NotFound().json(response)
+            },
+            TaskServiceError::TaskAlreadyExists => {
+                HttpResponse::Conflict().json(response)
+            }
         }
     }
 }
--- src/presentation/web_controller.rs
+++ src/presentation/web_controller.rs
@@ -1,9 +1,9 @@
 use std::sync::Mutex;
 
-use actix_web::{delete, get, patch, post, web, HttpResponse, Responder};
+use actix_web::{HttpResponse, Responder, delete, get, patch, post, web};
 
-use crate::application::services::{TaskService, TaskServiceError};
-use crate::presentation::dto::{TaskResponse, CreateTaskRequest};
+use crate::application::services::TaskService;
+use crate::presentation::dto::{CreateTaskRequest, TaskResponse};
 
 #[get("/tasks")]
 pub async fn get_all_tasks(task_service: web::Data<Mutex<TaskService>>) -> impl Responder {
@@ -14,77 +14,83 @@ pub async fn get_all_tasks(task_service: web::Data<Mutex<TaskService>>) -> impl 
 }
 
 #[get("/task/{id}")]
-pub async fn get_task_by_id(task_service: web::Data<Mutex<TaskService>>, id: web::Path<u32>) -> impl Responder {
+pub async fn get_task_by_id(
+    task_service: web::Data<Mutex<TaskService>>,
+    id: web::Path<u32>
+) -> impl Responder {
     println!("get_task_by_id/{id}");
-    match task_service.lock().unwrap().get_by_id(*id) {
-        Ok(task) => {
-            println!("Task found");
-            let response = TaskResponse::from(task);
-            HttpResponse::Ok().json(response)
+    match task_service
+        .lock()
+        .unwrap()
+        .get_by_id(*id) {
+            Ok(task) => {
+                println!("response: {:?}", task);
+                let response = TaskResponse::from(task);
+                HttpResponse::Ok().body(serde_json::to_string_pretty(&response).unwrap())
+            }
+            Err(e) => {
+                println!("Task not found");
+                HttpResponse::from(e)
+            },
         }
-        Err(_) => {
-            println!("Task not found");
-            HttpResponse::NotFound().body("Task not found")
-        },
-    }
 }
 
 #[post("/tasks")]
-pub async fn create_task(task_service: web::Data<Mutex<TaskService>>, request: web::Json<CreateTaskRequest>) -> impl Responder {
-    println!("create_task title: {}, description: {}", request.title, request.description);
-    match task_service.lock().unwrap().create(request.title.to_string(), request.description.to_string()) {
-        Ok(_) => {
-            println!("Task created");
-            HttpResponse::Ok().body("Task created")},
-        Err(e) => {
-            match e {
-                TaskServiceError::MissingTitle => {
-                    println!("Missing title");
-                    HttpResponse::BadRequest().body("Missing title")
-                },
-                TaskServiceError::MissingDescription => {
-                    println!("Missing description");
-                    HttpResponse::BadRequest().body("Missing description")
-                },
-                TaskServiceError::TaskAlreadyExists => {
-                    println!("Task already exists");
-                    HttpResponse::BadRequest().body("Task already exists")
-                },
-                _ => {
-                    println!("Unknown error");
-                    HttpResponse::InternalServerError().body("Unknown error")
-                }
+pub async fn create_task(
+    task_service: web::Data<Mutex<TaskService>>,
+    request: web::Json<CreateTaskRequest>,
+) -> impl Responder {
+    println!(
+        "create_task title: {}, description: {}",
+        request.title, request.description
+    );
+    match task_service
+        .lock()
+        .unwrap()
+        .create(request.title.to_string(), request.description.to_string()) {
+            Ok(_) => {
+                println!("Task created");
+                HttpResponse::Ok().json("Task created")
             }
-        },
-    }
+            Err(e) => {
+                println!("Error creating task");
+                HttpResponse::from(e)
+            }
+        }
 }
 
 #[patch("/tasks/{id}")]
-pub async fn toggle_task(task_service: web::Data<Mutex<TaskService>>, id: web::Path<u32>) -> impl Responder {
+pub async fn toggle_task(
+    task_service: web::Data<Mutex<TaskService>>,
+    id: web::Path<u32>,
+) -> impl Responder {
     println!("toggle_task/{id}");
     match task_service.lock().unwrap().toggle(*id) {
         Ok(_) => {
             println!("Task toggled");
-            HttpResponse::Ok().body("Task toggled")
-        },
-        Err(_) => {
+            HttpResponse::Ok().json("Task toggled")
+        }
+        Err(e) => {
             println!("Task not found");
-            HttpResponse::NotFound().body("Task not found")
-        },
+            HttpResponse::from(e)
+        }
     }
 }
 
 #[delete("/tasks/{id}")]
-pub async fn delete_task(task_service: web::Data<Mutex<TaskService>>, id: web::Path<u32>) -> impl Responder {
+pub async fn delete_task(
+    task_service: web::Data<Mutex<TaskService>>,
+    id: web::Path<u32>,
+) -> impl Responder {
     println!("delete_task/{id}");
     match task_service.lock().unwrap().delete(*id) {
         Ok(_) => {
             println!("Task deleted");
-            HttpResponse::Ok().body("Task deleted")
-        },
-        Err(_) => {
+            HttpResponse::Ok().json("Task deleted")
+        }
+        Err(e) => {
             println!("Task not found");
-            HttpResponse::NotFound().body("Task not found")
-        },
+            HttpResponse::from(e)
+        }
     }
 }
